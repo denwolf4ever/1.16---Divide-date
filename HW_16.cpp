29ec6a4a3557aa2d98e70a5064250afbf3d96bc1
denwolf4ever <denwolf4ever@gmail.com>
2024-04-11 13:33:03 +0300

    first commit

---
+++ HW_16.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <time.h>
+using namespace std;
+
+int main()
+{
+	const int length = 5;
+	int array[length][length];
+	for (int i = 0; i < length; i++)
+	{
+		for (int j = 0; j < length; j++)
+		{
+			array[i][j] = i + j;
+		}
+	}
+	for (int i = 0; i < length; i++)
+	{
+		for (int j = 0; j < length; j++)
+		{
+			cout << array[i][j] << ' ';
+		}
+		cout << endl;
+	}
+	struct tm buf;
+	time_t t = time(NULL);
+	localtime_s(&buf, &t);
+	for (int i = 0; i < length; i++)
+	{
+		cout << array[buf.tm_mday%length][i] << ' ';
+	}
+}
+
