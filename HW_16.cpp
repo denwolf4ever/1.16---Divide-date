ace50103e855b73ac7b3846b9ed52ec5f4f07945
denwolf4ever <denwolf4ever@gmail.com>
2024-04-23 17:49:09 +0300

    Updated
    Less cycles

--- HW_16.cpp
+++ HW_16.cpp
@@ -4,13 +4,22 @@ using namespace std;
 
 int main()
 {
+	struct tm buf;
+	time_t t = time(NULL);
+	localtime_s(&buf, &t);
+	int sum = 0;
 	const int length = 5;
 	int array[length][length];
 	for (int i = 0; i < length; i++)
 	{
+		
 		for (int j = 0; j < length; j++)
 		{
 			array[i][j] = i + j;
+			if (i == buf.tm_mday % length)
+			{
+				sum += array[i][j];
+			}
 		}
 	}
 	for (int i = 0; i < length; i++)
@@ -21,14 +30,11 @@ int main()
 		}
 		cout << endl;
 	}
-	struct tm buf;
-	time_t t = time(NULL);
-	localtime_s(&buf, &t);
-	int sum = 0;
-	for (int i = 0; i < length; i++)
+	
+	/*for (int i = 0; i < length; i++)
 	{
 		sum += array[buf.tm_mday%length][i];
-	}
+	}*/
 	cout << sum << endl;
 }
 
